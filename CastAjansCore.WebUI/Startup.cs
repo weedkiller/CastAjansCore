c57159df54aad71b18bca5496ebb22c6a30a4eb8
ondercalbay <ondercalbay@hotmail.com>
2019-05-14 22:07:43 +0300

    starup

--- CastAjansCore.WebUI/Startup.cs
+++ CastAjansCore.WebUI/Startup.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using CastAjansCore.DataLayer.EntityFramework;
+﻿using CastAjansCore.DataLayer.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace CastAjansCore.WebUI
 {
@@ -18,11 +15,18 @@ namespace CastAjansCore.WebUI
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            string cnnStr = "Data Source=94.73.146.4;Initial Catalog=u7506792_CastAja;Persist Security Info=True;User ID=u7506792_CastAja;Password=albay69sFENDER";
-            services.AddDbContext<CastAjansContext>(option => option.UseSqlServer(cnnStr));
+            
+            services.AddDbContext<CastAjansContext>(option => option.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
 
             services.AddSession();
             services.AddMemoryCache();
