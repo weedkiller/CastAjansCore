ac4317549499ae85e3375fc72251941d9dcc3c94
ondercalbay <ondercalbay@hotmail.com>
2019-05-27 01:59:01 +0300

    iller

--- CastAjansCore.WebUI/Controllers/IllerController.cs
+++ CastAjansCore.WebUI/Controllers/IllerController.cs
@@ -3,6 +3,7 @@ using CastAjansCore.DataLayer.Abstract;
 using CastAjansCore.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading.Tasks;
 
 namespace CastAjansCore.WebUI.Controllers
@@ -48,7 +49,30 @@ namespace CastAjansCore.WebUI.Controllers
             return View(entity);
         }
 
+        // GET: Ils/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
+        // GET: Ils/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Il());
+            }
+            else
+            {
+                var entity = await _IlServis.GetByIdAsync(id.Value);
+                if (entity == null)
+                {
+                    return NotFound();
+                }
+
+                return View(entity);
+            }
+        }
 
         // POST: Ils/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
@@ -63,9 +87,15 @@ namespace CastAjansCore.WebUI.Controllers
             {
                 try
                 {
+                    Il.GuncelleyenId = 1;
+                    Il.GuncellemeZamani = DateTime.Now;
+                    Il.Aktif = true;
 
-                    if (id == null)
+                    if (id == null || id == 0)
                     {
+                        Il.EkleyenId = 1;
+                        Il.EklemeZamani = DateTime.Now;
+                        
                         await _IlServis.AddAsync(Il);
                     }
                     else
